93ef7ef1b301dc0554fe06488f61fc640880350f
SHERCAN <sidereoyess@gmail.com>
2023-02-09 08:02:57 -0500

    Update columns form

--- static/js/index.js
+++ static/js/index.js
@@ -1,14 +1,27 @@
 function myFunc() {
-  let numInput = document.getElementById("numInput");
+  let numInput = document.getElementById("tableData");
   while (numInput.nextSibling) {
     numInput.nextSibling.remove();
   }
 
   let numInputval = document.getElementById("numInput").value;
-  for (var i = numInputval; i > 0; i--) {
+  for (var i = 1; i <= numInputval; i++) {
     var newDiv = document.createElement("div");
-    newDiv.setAttribute("id", "demo" + i);
-    newDiv.innerHTML = "*** TEST ***";
-    numInput.parentNode.insertBefore(newDiv, numInput.nextSibling);
+    newDiv.setAttribute("class", "d-flex align-items-center ");
+    newDiv.innerHTML = ` <div>
+                                    <input class="number form-control" type="number" value="${i}" readonly>
+                                </div>
+                                <div>
+                                    <input class="time form-control" name="starttime${i}" type="time">
+                                </div>
+                                <div>
+                                    <input class="number form-control" name="finaldate" type="number" id="duration${i}"
+                                        placeholder="1">
+                                </div>
+                                <div>
+                                    <input class="number form-control" name="finaldate" type="number" id="duration${i}"
+                                        placeholder="100">
+                                </div>`;
+    numInput.insertBefore(newDiv, numInput.nextSibling);
   }
 }
